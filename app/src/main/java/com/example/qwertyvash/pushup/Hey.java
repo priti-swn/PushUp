6c9ead185c7d1182a8b62536b47527e6efcb69f5
priti-swn <pritivash.swain.1234@gmail.com>
2018-03-08 01:10:14 +0530

    Modified activity

---
+++ app/src/main/java/com/example/qwertyvash/pushup/Hey.java
@@ -0,0 +1,8 @@
+package com.example.qwertyvash.pushup;
+
+/**
+ * Created by QWERTYvash on 3/8/2018.
+ */
+
+public class Hey {
+}
---
+++ app/src/main/java/com/example/qwertyvash/pushup/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.qwertyvash.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestUpload()
+    {
+
+        int i =1+1;
+     i++;
+    }
+}
